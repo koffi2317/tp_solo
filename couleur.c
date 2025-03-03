b96a8663c2b4f50a57e9b0ea6b1344a338e4be5e
Olivier77-kenzo <ofrancois67@gmail.com>
2025-03-03 11:55:46 -0500

    couleur.c

--- couleur.c
+++ couleur.c
@@ -1,1 +1,15 @@
 /* je vais transferer mon code établit dans clion directement sur GitHub*/
+/* le modifier aussi!*/
+#include "couleur.h"
+
+#include <stdio.h>
+
+#define ESCAPE "\033["
+
+void couleur_set(int style, int couleur_avant_plan, int couleur_arriere_plan) {
+    printf("%s%d;%d;%dm", ESCAPE, style, couleur_avant_plan, couleur_arriere_plan);
+}
+
+void couleur_reset(void) {
+    printf("%s0m", ESCAPE);
+}
