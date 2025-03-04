3989af55a6ae2011cd5ab349d9b1f52d2fa538d7
Olivier77-kenzo <ofrancois67@gmail.com>
2025-03-03 19:50:31 -0500

    jeu.c

--- jeu.c
+++ jeu.c
@@ -7,9 +7,46 @@
 //  ***********************************
 
 // Definir la fonction 'jeu_maj_carburant_joueur' ici
+void jeu_maj_carburant_joueur(int joueur_ligne, int joueur_colonne, int *joueur_carburant, Terrain *) {
+
+    *joueur_carburant += (terrain=carburant[joueur_ligne][joueur_colonne]);
+    terrain=(carburant[joueur_ligne][joueur_colonne] = 0);
+    *joueur_carburant -= COUT_DEPLACEMENT_VOISIN;
+}
+
 
 // Definir la fonction 'jeu_deplacer_joueur' ici
+bool jeu_deplacer_joueur(int *joueur_ligne, int *joueur_colonne, t_direction direction) {
+    int ligne_supplementaire = *joueur_ligne, colonne_supplementaire = *joueur_colonne;
 
+    switch (direction) {
+        case DIRECTION_HAUT:
+          ligne_supplementaire--;
+          break;
+
+        case DIRECTION_BAS:
+          ligne_supplementaire++;
+          break;
+
+        case DIRECTION_GAUCHE:
+          colonne_supplementaire--;
+          break;
+
+        case DIRECTION_DROITE:
+          colonne_supplementaire++;
+          break;
+
+        default:
+          return false;
+    }
+
+    if (nouvelle_ligne >= 0 && nouvelle_ligne < 10 && nouvelle_colonne >= 0 && nouvelle_colonne < 10) {
+        *joueur_ligne =  ligne_supplementaire;
+        *joueur_colonne = colonne_supplementaire;
+        return true;
+    }
+    return false;
+}
 // Definir la fonction 'jeu_init' ici
 
 // Definir la fonction 'jeu_afficher_direction' ici
