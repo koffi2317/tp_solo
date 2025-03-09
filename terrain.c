9a5b7b305f816f220945f86fa7b9cb8879be59fc
WassimOuali <wassim.ouali.1@ens.etsmtl.ca>
2025-03-09 12:02:34 -0400

    modification fonction: -position départ -terrain afficher -terrain get carburant
    A verifier:
    -utilisation des pointeurs
    -constante en parametre

--- terrain.c
+++ terrain.c
@@ -10,56 +10,32 @@
 //  ***********************************
 //  Definitions des fonctions publiques
 //  ***********************************
+
 // Definir la fonction 'terrain_generer_position_sortie' ici
 
 void terrain_generer_position_sortie(int *sortie_colonne, int *sortie_ligne) {
     *sortie_colonne = util_generer_nombre_aleatoire(0, (NB_COLONNES - 1));
-    *sortie_ligne = util_generer_nombre_aleatoire(0, (NB_COLONNES - 1));
+    *sortie_ligne = util_generer_nombre_aleatoire(0, (NB_LIGNES - 1));
 }
 
 
 // Definir la fonction 'terrain_generer_position_depart' ici
 
 void terrain_generer_position_depart(int destination_ligne, int destination_colonne, int
-                                     *depart_lignes, int *depart_colonne) {
+*depart_lignes, int *depart_colonne) {
     int distance_tot = 0;
     int distance_ligne = 0;
     int distance_colonne = 0;
-    *depart_lignes = util_generer_nombre_aleatoire(0, (NB_COLONNES - 1));
-    *depart_colonne = util_generer_nombre_aleatoire(0, (NB_COLONNES - 1));
 
-    distance_ligne = destination_ligne - *depart_lignes;
-    distance_colonne = destination_colonne - *depart_colonne;
+    do {
+        *depart_lignes = util_generer_nombre_aleatoire(0, NB_LIGNES - 1);
+        *depart_colonne = util_generer_nombre_aleatoire(0, NB_COLONNES - 1);
 
-    if (distance_ligne < 0) {
-        distance_ligne *= -1;
-    }
-    if (distance_colonne < 0) {
-        distance_colonne *= -1;
-    }
+        distance_ligne = abs(destination_ligne - *depart_lignes);
+        distance_colonne = abs(destination_colonne - *depart_colonne);
+        distance_tot = distance_ligne + distance_colonne;
 
-    distance_tot = distance_ligne + distance_colonne;
-
-    if (distance_tot < DISTANCE_MIN) {
-        do {
-            *depart_lignes = util_generer_nombre_aleatoire(
-                (NB_LIGNES - 1), (NB_COLONNES - 1));
-            *depart_colonne = util_generer_nombre_aleatoire(
-                NB_LIGNES - 1, (NB_COLONNES - 1));
-
-            distance_ligne = destination_ligne - *depart_lignes;
-            distance_colonne = destination_colonne - *depart_colonne;
-
-            if (distance_ligne < 0) {
-                distance_ligne *= -1;
-            }
-            if (distance_colonne < 0) {
-                distance_colonne *= -1;
-            }
-
-            distance_tot = distance_ligne + distance_colonne;
-        } while (distance_tot < DISTANCE_MIN);
-    }
+    } while (distance_tot < DISTANCE_MIN);
 }
 
 // Definir la fonction 'terrain_contient' ici
@@ -77,16 +53,10 @@ int terrain_contient(int position_ligne, int position_colonne) {
 // Definir la fonction 'terrain_get_carburant' ici
 
 int terrain_get_carburant(int terrain[NB_LIGNES][NB_COLONNES], int position_ligne, int
-                          position_colonne) {
+position_colonne) {
     int carburant = 0;
 
-    for (int i = 0; i < NB_LIGNES; i++) {
-        for (int j = 0; j < NB_COLONNES; j++) {
-            if (i == position_ligne && j == position_colonne) {
-                carburant = terrain[i][j];
-            }
-        }
-    }
+    carburant = terrain[position_ligne][position_colonne];
 
     return carburant;
 }
@@ -95,11 +65,10 @@ int terrain_get_carburant(int terrain[NB_LIGNES][NB_COLONNES], int position_lign
 // Definir la fonction 'terrain_set_carburant' ici
 /*a ne pas oublier! tres important!*/
 
-void terrai_set_carburant(int terrain[NB_LIGNES][NB_COLONNES],int position_ligne, int
+void terrain_set_carburant(int terrain[NB_LIGNES][NB_COLONNES],int position_ligne, int
 position_colonne, int carburant) {
 
-terrain[position_ligne][position_colonne]=carburant;
-
+    terrain[position_ligne][position_colonne] = carburant;
 }
 
 // Definir la fonction 'terrain_init' ici
@@ -107,7 +76,7 @@ terrain[position_ligne][position_colonne]=carburant;
 void terrain_init(int terrain[NB_LIGNES][NB_COLONNES]) {
     for (int i = 0; i < NB_LIGNES; i++) {
         for (int j = 0; j < NB_COLONNES; j++) {
-            terrain[i][j] = NULL;
+            terrain[i][j] = 0;
         }
     }
 }
@@ -115,7 +84,7 @@ void terrain_init(int terrain[NB_LIGNES][NB_COLONNES]) {
 // Definir la fonction 'terrain_creer_stations_carburant' ici
 
 void terrain_creer_stations_carburant(int terrain[NB_LIGNES][NB_COLONNES], int
-                                      quantité_globale) {
+quantité_globale) {
     int carburant = 0;
 
     while (quantité_globale > 0) {
@@ -133,24 +102,26 @@ void terrain_creer_stations_carburant(int terrain[NB_LIGNES][NB_COLONNES], int
             }
             terrain[i][j] = carburant;
         }
-
         quantité_globale -= carburant;
     }
 }
 
 // Definir la fonction 'terrain_afficher' ici
 
-void terrain_afficher(int terrain[NB_LIGNES][NB_COLONNES], int courante_ligne,int
-courante_colonne,int destination_ligne, int destination_colonne) {
+void terrain_afficher(int terrain[NB_LIGNES][NB_COLONNES], int courante_ligne, int courante_colonne,
+                      int destination_ligne, int destination_colonne) {
+    printf("\t0\t1\t2\t3\t4\t5\t6\t7\t8\t9\t\n");
     for (int i = 0; i < NB_LIGNES; i++) {
+        printf("%i\t",i);
         for (int j = 0; j < NB_COLONNES; j++) {
-            if (i==courante_ligne && j==courante_colonne) {
-                printf("%i"); // avec couleur a revenir apres
-            } else if (i == destination_colonne && j == destination_colonne) {
-                printf(""); //avec couleur a revenir apres
+            if (i == courante_ligne && j == courante_colonne) {
+                printf("X\t");
+            } else if (i == destination_ligne && j == destination_colonne) {
+                printf("S\t");
             } else {
                 printf("%i\t", terrain[i][j]);
             }
         }
+        printf("\n");
     }
 }
