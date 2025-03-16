a207fe2daf4534dce2bdb104d1f83cb09c28fcfd
Olivier77-kenzo <ofrancois67@gmail.com>
2025-03-15 20:54:41 -0400

    finalisation.c

--- dijkstra.c
+++ dijkstra.c
@@ -13,6 +13,7 @@
 #include <stdio.h>
 #define INFINITY 9
 #define NB_DIRECTIONS 4
+
 typedef int t_couts[NB_LIGNES][NB_COLONNES];
 
 //  Type pour le tableau 2D permettant de savoir si une case a deja ete visitee lors de l'algorithme de Dijkstra
@@ -147,7 +148,131 @@ void afficher_direction_suggeree(int directions[]);
 //  ***********************************
 //  Definitions des fonctions publiques
 //  ***********************************
-// Definir la fonction 'dijkstra_acheter_bonus' ici
+
+// Le principe de case représente une position dans le terrain, selon la ligne, la colonne et le carburant.
+typedef struct {
+    int ligne, colonne, carburant;
+} Case;
+//elle est constitué des coordonnées verticale, horizontale ainsi que le cout attribué a une case
+
+// Les différents chemin qu'un utlisateur peut emprunter est représenter par "haut, bas, gauche, droite"
+int directions[4][2] = {{-1, 0}, {1, 0}, {0, -1}, {0, 1}};
+//ce tableau détient chaque direction possible pour un utilisateur. elles sont liées a
+//la chaine de caractère qui contient les lettres associés a ces déplacement
+char Charact_direction[4] = {"H","B","G","D"};
+
+//La fonction qui non seulement permet a l'utilisateur d'acheter un bonus mais construit également le chemin optimal.
+void dijkstra_acheter_bonus(int terrain[NB_LIGNES][NB_COLONNES], int joueur_ligne, int joueur_colonne, int *joueur_carburant, int destination_ligne, int destination_colonne) {
+
+  // Vérifier si le joueur a assez de carburant pour obtenir ou non le bonus
+    if (*joueur_carburant < 10) {
+        printf("Vous ne disposez pas assez de carburant afin d'acheter un bonus\n");
+        printf("Veillez accumuler plus de carburant et réessayer plus tard!\n");
+    }else if (*joueur_carburant > 10) {
+
+    // Lorsque le joueur décide d'obtenir le bonus, 10L de carburant lui est déduit
+ char choix_utilisateur;
+
+   printf("Vous avez 10 litres de carburant dans votre réservoir.\n");
+   printf("Voulez-vous vous procurez le bonus? veuillez répondre par O ou N:\n");
+   scanf("%c", &choix_utilisateur);
+
+   if (choix_utilisateur == 'N') {
+     printf("Parfait! le bonus ne vous sera pas attribué!\n");
+   } else if(choix_utilisateur == '0') {
+
+    *joueur_carburant -= 10;
+    printf("Vous venez d'acheter le bonus. Le carburant restant est de : %d Litres\n", *joueur_carburant);
+
+    //Le cout minimal pour le déplacement est stocké
+    int cout[NB_LIGNES][NB_COLONNES];
+    int direction_algo[NB_LIGNES][NB_COLONNES];
+    //Stockage du "chemin" pour se rendre a la case choisie
+
+
+    for (int i = 0; i < NB_LIGNES; i++) {
+        for (int j = 0; j < NB_COLONNES; j++) {
+
+          /*Changement du code concernant le stockage des couts*/
+
+           /* //Les couts de chaque cases sont initialiser aleatoirement
+            cout[i][j] = INF; /*a changer pour aleatoire*/
+
+            direction_algo[i][j] = -1;
+            //puisqu'il y a aucun dhemin prédéfinis pour le moment, les directions de l'algo sont a -1
+            //L'indicateur -1 réprésente toute cases pas atteintes et la cases de départ
+            //La case de Haut est 0, 1 réprésente les case de bas, 2 pour gauche et 3 pour droite
+        }
+    }
+
+    //Le tableau file sert a établir un ordre pour l'exploration des cases
+    Case file[NB_LIGNES * NB_COLONNES];
+    //Les indices du tout premier et dernier éléments a traiter dans la file des déplacement
+    int indice_debut_file = 0, indice_fin_file = 0;
+
+
+    file[indice_fin_file++] = (Case){joueur_ligne, joueur_colonne, 0};
+    cout[joueur_ligne][joueur_colonne] = 0;
+    //La case de l'utilisateur (au début du jeu) a un cout de ZÉRO
+    //Cette case est ajouté dans la file parmis les autres cases (comme première position)
+
+    // La fonction Dijkstra permet l'exploration des cases voisines a celle présentement occupée par l'utilisateur
+    while (indice_debut_file < indice_fin_file) {
+        Case actuel = file[indice_debut_file++];
+    //La case occupé par l'utilisateur (perçus) comme la premiere position dans la file des déplacement
+    //Toutes cases voisines accessibles selon les différents déplacements sont explorer
+
+    //Les 4 directions qu'un utilisateur peut emprunter son tester afin de déterminer le meilleur chemin a suivre
+        for (int mouvement = 0; mouvement < 4; mouvement++) {
+            int nouvelle_ligne = actuel.ligne + directions[mouvement][0];
+            int nouvelle_colonne = actuel.colonne + directions[mouvement][1];
+    //nouvelle ligne et colonne sont les coordonnées de la case voisine qui sera implimenter dans file
+
+            // Vérification de la case: Savoir si elle est éligible aux dimensions du terrain
+            if (nouvelle_ligne >= 0 && nouvelle_ligne < NB_LIGNES && nouvelle_colonne >= 0 && nouvelle_colonne < NB_COLONNES) {
+                int nouveau_cout = cout[actuel.ligne][actuel.colonne] + terrain[nouvelle_ligne][nouvelle_colonne];
+
+                //Le cout est Mise à jour si et uniquement si un meilleur chemin optimal est trouvé
+                if (nouveau_cout < cout[nouvelle_ligne][nouvelle_colonne]) {
+                    cout[nouvelle_ligne][nouvelle_colonne] = nouveau_cout;
+
+                    //Pour le chemin optimal, le chemin a suivre est maintenue dans la sirection de l'algorithme
+                    direction_algo[nouvelle_ligne][nouvelle_colonne] = mouvement;
+
+                    //Par la suite, cette meme case est enregistrer dans la file parmis les autres cases pour déterminer le chemin optimal
+                    file[indice_fin_file++] = (Case){nouvelle_ligne, nouvelle_colonne, nouveau_cout};
+                }
+            }
+        }
+    }
+
+    // Reconstruction du chemin menant a la sortie
+    int chemin[4];
+    int ligne = destination_ligne, colonne = destination_colonne;
+    int lexique_des_repères = 0;
+    //Le lexique sert a parcourir et enegistrer le chemin menant a la sortie
+
+    //Les cases qui sont enregistrés pour mener a la sortie sont explorer pour la constitution du chemin optimal
+    while (direction_algo[ligne][colonne] != -1){
+       if(lexique_des_repères < 4) {
+        int mouvement= direction_algo[ligne][colonne];
+
+        chemin[lexique_des_repères++] = mouvement;
+
+        ligne -= directions[mouvement][0];
+        colonne -= directions[mouvement][1];
+        }
+    }
+
+    // Les 4 directions qui seront affichées pour le chemin optimal
+    printf("Les 4 premieres directions pour le chemin optimal est : \n");
+    for (int i = lexique_des_repères - 1; i >= 0; i--) {
+        printf("%c\n", Charact_direction[chemin[i]]);
+      }
+     }
+    }
+}
+
 
 //  *********************************
 //  Definitions des fonctions privees
@@ -222,20 +347,6 @@ void maj_voisins(t_couts couts, t_visites visitees, t_terrain terrain, t_precede
     }
 }
 
-/**
- * @brief Algorithme de Dijkstra pour calculer le plus court chemin en partent de la position du joueur pour atteindre
- * la position de destination.
- * @name dijkstra
- * @param terrain la terrain sur lequel le joueur evolue
- * @param joueur_ligne la ligne ou se trouve le joueur
- * @param joueur_colonne la colonne ou se trouve le joueur
- * @param destination_ligne la ligne de la case a atteindre
- * @param destination_colonne la colonne de la case a atteindre
- * @param directions les 4 premieres directions a suivre sur le plus court chemin
- * @return
- */
-//  Ecrire le prototype de la fonction 'dijkstra' ici
-
  void dijkstra(t_terrain terrain, int joueur_ligne, int joueur_colonne, int destination_ligne, int destination_colonne, t_precedents precedents) {
     t_couts couts;
     t_visites visitees;
@@ -289,7 +400,6 @@ void afficher_couts(t_couts couts, t_visites visitees, int joueur_ligne, int jou
             }
         }
     }
-    // printf(tab couts)
 }
 
 void afficher_direction_suggeree(int directions[]) {
@@ -298,9 +408,3 @@ void afficher_direction_suggeree(int directions[]) {
         printf("%d ", directions[i]);
     }
 }
-/**
- * @brief Affiche les 4 premieres directions a suivre sur le plus court chemin pour joindre la destination a partir de la position du joueur
- * @name afficher_direction_suggeree
- * @param dircetions les premieres directions a suivre sur le plus court chemin
- * @param nb le nombre de directions a suivre
- */
